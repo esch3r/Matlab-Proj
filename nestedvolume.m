aa39ec9dcf3d5d7b203fe5411f4fe6d81e6e1da1
Johnathan Machler <32213433+esch3r@users.noreply.github.com>
2018-11-07 02:28:54 -0600

    Add files via upload

---
+++ nestedvolume.m
@@ -0,0 +1,9 @@
+function outvol = nestedvolume(len, wid, ht) 
+  % demostrates a nested function 
+  
+  outvol = base*ht; 
+  function outbase = base 
+    outbase = len*wid; 
+   
+  endfunction % base function 
+endfunction % nested volume function 
