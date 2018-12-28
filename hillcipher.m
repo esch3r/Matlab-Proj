2359c61066b89c923383d33d10c9b5a4a7df4ace
Johnathan Machler <32213433+esch3r@users.noreply.github.com>
2018-12-27 22:11:39 -0600

    Add files via upload

---
+++ hillcipher.m
@@ -0,0 +1,11 @@
+function [code] = hillcipher(stringn,key)
+  Msg= double(stringn)-64; 
+  [m,n]= size(key);
+  [j,k]= size(Msg);  
+  
+  Msg = reshape(Msg,[n,k])
+  key = reshape(key,[2,3]); 
+  Encrypt= key*Msg;
+  Encrypt = mod(Encrypt,26); 
+endfunction
+
