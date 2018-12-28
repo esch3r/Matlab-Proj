f059622ee658db9a2eb32d73f191a9f3c3f5428d
Johnathan Machler <32213433+esch3r@users.noreply.github.com>
2018-12-27 19:49:53 -0600

    Add files via upload

---
+++ Fizzbuzz.m
@@ -0,0 +1,14 @@
+%fizzbuzz for two abitary numbers
+
+function [Counter,Q] = Fizzbuzz(n,i,j)
+Counter =0;
+     for h=1:n
+       if  mod(h,i) ==0
+         disp(h);
+         Counter=Counter+1;
+       elseif mod(h,j)==0
+         disp(h);
+         Counter=Counter+1;
+       endif
+     endfor
+endfunction
